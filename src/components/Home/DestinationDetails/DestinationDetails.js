f09cc491e30d22ebf4940d79977fb881422c4d0c
shakilibn <mdsh540@gmail.com>
2021-04-18 18:03:21 +0600

    destination section done

--- src/components/Home/DestinationDetails/DestinationDetails.js
+++ src/components/Home/DestinationDetails/DestinationDetails.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
+import './DestinationDetails.css';
 
 const DestinationDetails = ({destination}) => {
     return (
         <div className="col-md-4">
-            <div className=" d-flex justify-content-center">
-                <Card className="mx-2 my-3 shadow" style={{ width: '22rem' }}>
+            <div className=" d-flex justify-content-center destination-card">
+                <Card className="mx-2 my-3 shadow destination-card" style={{ width: '22rem', overflow: 'hidden' }}>
                     <Card.Img variant="top" style={{ height: '250px' }} src={destination.imageURL} />
-                    <Card.Body>
+                    <Card.Body style={{zIndex:'1', backgroundColor:'white'}}>
                         <Card.Title>{destination.name}</Card.Title>
                         <Card.Text><small><span className="text-secondary">{destination.description}</span></small></Card.Text>
                     </Card.Body>
--- src/components/Home/Destinations/Destinations.js
+++ src/components/Home/Destinations/Destinations.js
@@ -1,25 +1,41 @@
+import { Button } from 'react-bootstrap';
 import React, { useEffect, useState } from 'react';
 import DestinationDetails from '../DestinationDetails/DestinationDetails';
 
 const Destinations = () => {
+    const [slicedServices, setSlicedServices] = useState([]);
     const [services, setServices] = useState([]);
+    const [isExplore, setIsExplore] = useState(false);
 
     useEffect(() => {
         fetch("https://blooming-hollows-97264.herokuapp.com/destinations")
-        .then(res => res.json())
-        .then(data => setServices(data))
-    },[])
+            .then(res => res.json())
+            .then(data => {
+                setServices(data)
+                const newData = data.slice(0, 3);
+                setSlicedServices(newData)
+            })
+    }, [])
     return (
         <section className="container d-flex justify-content-around mt-5 pt-4 text-center">
             <div>
                 <h4 className="text-center brand-text2">Places</h4>
                 <h1 className="text-center mb-4">Hundreds of...<span className="brand-text">Destinations Worldwide.</span> </h1>
                 <p className="text-center mb-4 text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo libero odit a doloribus dignissimos quos, sequi itaque in enim optio?</p>
-                <div className="row">
-                    {
-                        services.map(destination => <DestinationDetails key={destination._id} destination={destination}/>)
-                    }
+                <div>
+                    {isExplore ?
+                        <div className="row">
+                            {
+                                services.map(destination => <DestinationDetails key={destination._id} destination={destination} />)
+                            }
+                        </div> :
+                        <div className="row">
+                            {
+                                slicedServices.map(destination => <DestinationDetails key={destination._id} destination={destination} />)
+                            }
+                        </div>}
                 </div>
+                {!isExplore && <Button onClick={() => setIsExplore(true)} className="brand-btn mt-5">Explore More</Button>}
             </div>
         </section>
     );
