32f692bcb1dbb22c965b1b6134872058625d1f95
shakilibn <mdsh540@gmail.com>
2022-01-18 23:32:02 +0600

    dollar sign changed

--- src/components/Home/ServiceDetails/ServiceDetails.js
+++ src/components/Home/ServiceDetails/ServiceDetails.js
@@ -1,29 +1,45 @@
-import React from 'react';
-import { Button, Card } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import './ServiceDetails.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./ServiceDetails.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
 
 const ServiceDetails = ({ service }) => {
-    
-    return (
-        <div className="col-md-4">
-            <div className=" d-flex justify-content-center service-card">
-                <Card className="mx-2 my-3 shadow service-card" style={{ width: '20rem', overflow: 'hidden' }}>
-                    <Card.Img variant="top" style={{ height: '220px' }} src={service.imageURL} />
-                    <Card.Body style={{zIndex:'1', backgroundColor:'white'}}>
-                        <Card.Title>{service.name}</Card.Title>
-                        <Card.Text><FontAwesomeIcon className="text-secondary" icon={faGlobe} /><span className="text-secondary"> {service.description}</span></Card.Text>
-                        <div className="d-flex justify-content-between">
-                            <h2 className="brand-text">${service.price}</h2>
-                            <Button className="brand-btn" as={Link} to={`/service/${service._id}`} variant="primary">Book Now</Button>
-                        </div>
-                    </Card.Body>
-                </Card>
+  return (
+    <div className="col-md-4">
+      <div className=" d-flex justify-content-center service-card">
+        <Card
+          className="mx-2 my-3 shadow service-card"
+          style={{ width: "20rem", overflow: "hidden" }}
+        >
+          <Card.Img
+            variant="top"
+            style={{ height: "220px" }}
+            src={service.imageURL}
+          />
+          <Card.Body style={{ zIndex: "1", backgroundColor: "white" }}>
+            <Card.Title>{service.name}</Card.Title>
+            <Card.Text>
+              <FontAwesomeIcon className="text-secondary" icon={faGlobe} />
+              <span className="text-secondary"> {service.description}</span>
+            </Card.Text>
+            <div className="d-flex justify-content-between">
+              <h2 className="brand-text">{service.price}</h2>
+              <Button
+                className="brand-btn"
+                as={Link}
+                to={`/service/${service._id}`}
+                variant="primary"
+              >
+                Book Now
+              </Button>
             </div>
-        </div>
-    );
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceDetails;
